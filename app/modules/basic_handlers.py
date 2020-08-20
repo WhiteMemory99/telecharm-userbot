5effce309ec60abc55e16fe5f3d1dc924229715a
WhiteMemory99 <lisen1510@gmail.com>
2020-08-21 01:39:17 +0700

    More .purge improvements(ffs here we go again)

--- app/modules/basic_handlers.py
+++ app/modules/basic_handlers.py
@@ -24,22 +24,27 @@ async def flood_handler(_, message: Message):
 @Client.on_message(Filters.me & Filters.reply & Filters.command('purge', prefixes='.'))
 async def purge_handler(client: Client, message: Message):
     args = message.text.split(maxsplit=2)
-    me_mode = True if len(args) > 1 and 'me' in args else False
+    me_mode = True if 'me' in args else False
 
-    if message.chat.type in ['group', 'supergroup'] and len(args) > 1:
+    if message.chat.type in ['group', 'supergroup'] and not me_mode:
         member = await message.chat.get_member(message.from_user.id)
-        if not member.can_delete_messages and member.status != 'creator' and not me_mode:
+        if not member.can_delete_messages and member.status != 'creator':
             me_mode = True
     elif message.chat.type == 'channel':
         return  # TODO: Think about channels
 
     msgs = []
-    async for msg in client.iter_history(message.chat.id, offset_id=message.reply_to_message.message_id, reverse=True):
+    async for msg in client.iter_history(  # noqa
+            message.chat.id,
+            offset=1,
+            offset_id=message.reply_to_message.message_id,
+            reverse=True):
         if me_mode and msg.from_user.id != message.from_user.id:
             continue
         else:
             msgs.append(msg.message_id)
 
+    logger.info(len(msgs))
     try:
         await client.delete_messages(message.chat.id, message_ids=msgs)
     except RPCError as ex:
