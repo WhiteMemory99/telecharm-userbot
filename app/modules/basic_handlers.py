e2b56546418a79c753efe135f0fa2ed0b0193099
WhiteMemory99 <lisen1510@gmail.com>
2020-08-21 01:23:44 +0700

    .purge fixes and better error handling

--- app/modules/basic_handlers.py
+++ app/modules/basic_handlers.py
@@ -1,5 +1,6 @@
 from loguru import logger
 from pyrogram import Client, Filters, Message
+from pyrogram.errors import RPCError
 
 
 @Client.on_message(Filters.me & Filters.command('flood', prefixes='.'))
@@ -15,7 +16,7 @@ async def flood_handler(_, message: Message):
         for _ in range(int(args[1])):
             try:
                 await message.reply_text(args[2], quote=False, disable_web_page_preview=True)
-            except Exception as ex:
+            except RPCError as ex:
                 logger.error(f'{ex} in .flood, stopping command!')
                 break
 
@@ -23,7 +24,7 @@ async def flood_handler(_, message: Message):
 @Client.on_message(Filters.me & Filters.reply & Filters.command('purge', prefixes='.'))
 async def purge_handler(client: Client, message: Message):
     args = message.text.split(maxsplit=2)
-    me_mode = True if len(args) > 1 and args[1] == 'me' else False
+    me_mode = True if len(args) > 1 and 'me' in args else False
 
     if message.chat.type in ['group', 'supergroup'] and len(args) > 1:
         member = await message.chat.get_member(message.from_user.id)
@@ -34,13 +35,12 @@ async def purge_handler(client: Client, message: Message):
 
     msgs = []
     async for msg in client.iter_history(message.chat.id, offset_id=message.reply_to_message.message_id, reverse=True):
-        try:
-            if me_mode and msg.from_user.id != message.from_user.id:
-                continue
-            else:
-                msgs.append(msg.message_id)
-        except Exception as ex:
-            logger.error(f'{ex} in .purge, stopping command!')
-            break
+        if me_mode and msg.from_user.id != message.from_user.id:
+            continue
+        else:
+            msgs.append(msg.message_id)
 
-    await client.delete_messages(message.chat.id, message_ids=msgs)
+    try:
+        await client.delete_messages(message.chat.id, message_ids=msgs)
+    except RPCError as ex:
+        logger.error(f'Could not .purge messages due to {ex}')
