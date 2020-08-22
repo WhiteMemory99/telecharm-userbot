9c92d3cd11c9f331db7c75994c45095c533ed145
WhiteMemory99 <lisen1510@gmail.com>
2020-08-22 23:28:47 +0700

    A few text fixes

--- app/main.py
+++ app/main.py
@@ -18,10 +18,10 @@ async def help_handler(_, message: Message):
         text = f'**Telecharm v{config.TELECHARM_VERSION}**:\n\n`.help` - Показать это сообщение.\n' \
                '`.stats` - Статистика моего профиля.\n`.flood` - Флуд сообщениями.\n`.purge` - Очистка сообщений\n' \
                '`.name` - Сменить моё имя\n`.username` - Сменить мой юзернейм\n`.bio` - Отредактировать "О себе"\n' \
-               f'`.mention - Текстовое упоминание`\n\n__[Подробнее о командах]({config.GUIDE_LINK_RU})\n' \
+               f'`.mention` - Текстовое упоминание\n\n__[Подробнее о командах]({config.GUIDE_LINK_RU})\n' \
                f'[Telecharm на GitHub]({config.GITHUB_LINK})__'
     else:
-        text = f'**Telecharm {config.TELECHARM_VERSION}**:\n`.help ru` for Russian\n\n`.help` - Show this message.\n' \
+        text = f'**Telecharm v{config.TELECHARM_VERSION}**:\n`.help ru` for Russian\n\n`.help` - Show this message.\n' \
                '`.stats` - My profile stats.\n`.flood` - Flood messages.\n`.purge` - Purge messages\n' \
                '`.name` - Change my name\n`.username` - Change my username\n`.bio` - Edit my about info\n' \
                f'`.mention` - Text mention\n\n__[More on commands]({config.GUIDE_LINK_EN})\n' \
